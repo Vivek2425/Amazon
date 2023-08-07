344f3dac69c65acfc704b25532cdfc4ddaeb93fc
Vivek2425 <viveklakhlani.mscit20@vnsgu.ac.in>
2023-08-07 09:56:17 +0530

    login complete with mongodb

--- index.js
+++ index.js
@@ -2,7 +2,64 @@ const express = require("express");
 const app = express();
 const path = require('path');
 app.use(express.json());
-const static  = app.use(express.static('public'))
+const bodyParser = require('body-parser');
+app.use(express.static('public'))
+const { MongoClient } = require('mongodb');
+
+const client = new MongoClient('mongodb://localhost:27017');
+// async function connectToMongo() {
+//     try {
+//       await client.connect();
+//       console.log('Connected to MongoDB successfully.');
+      
+//     } catch (error) {
+//       console.error('Error connecting to MongoDB:', error);
+//     }
+//   }
+  
+//   connectToMongo();
+  async function commonFunc(DB,Collection,Data,ProcessFlag){
+    var data ;
+    try {
+        await client.connect();
+        const db = client.db(DB); // database name
+        const collection = db.collection(Collection); // collection name
+        switch(ProcessFlag){
+            case "insert":
+                const insertResult = await collection.insertOne(Data);
+                console.log('Inserted document:', insertResult.insertedId);
+                return insertResult.insertedId;
+                break;
+            case "display":
+                // const query = Data; 
+                // const allDocuments = await collection.find(query).toArray();
+                // console.log('All documents in the collection:', allDocuments);
+                // console.log(Data)
+                const query = Data;
+                // const matchingDocuments = await collection.find(query).toArray();
+                const matchingDocuments = await collection.findOne(query);
+                // console.log('Documents matching the query:', matchingDocuments);
+                // return matchingDocuments;
+                // if (matchingDocuments) {
+                //     // console.log('Data exists:', matchingDocuments);
+                // } else {
+                //     return false;
+                // }
+                return matchingDocuments;
+                break;
+            default:
+                client.close().then(() => {
+                    console.log('Connection to MongoDB closed successfully.');
+                  });
+                break;
+        }
+      } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+      }
+  }
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get("/",(req,res)=>{
     res.send("Hello world");
 })
@@ -10,15 +67,19 @@ app.get("/login",(req,res)=>{
     const filePath = path.join(__dirname, 'public/login.html');
     res.sendFile(filePath);
 })
-app.post('/auth', (req, res) => {
-    // const { email, password } = req.body;
-    console.log(req.body.email)
-    // You can now process the email and password data
-    // For authentication, you might want to validate the user's credentials
-    // against a database or some other authentication mechanism.
-  
-    // For now, let's just send a simple response with the received data:
-    res.json({ email, password });
+app.post('/auth',async (req, res) => {
+    const data = req.body;
+    // const id = commonFunc('ecommerce','products',data,'insert');
+    // console.log(id)
+    const valid =await commonFunc('ecommerce','products',data,'display');
+    // console.log(valid);
+    // if(valid!==false)
+    // {
+        res.end(JSON.stringify(valid));
+    // }else{
+    //     res.end("not valid")
+    // }
+    
   });
 app.listen(8000,()=>{
     console.log("http://localhost:8000")
